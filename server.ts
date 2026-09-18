1fba2ff3f454d24d2403a4f5fa5afea83d563f4b
shaikhabdul2015-max <shaikhabdul2015@gmail.com>
2026-09-18 11:05:07 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ import {
   toggleProjectFileEnabled,
   deleteProjectFile,
   buildProjectPromptContext,
-} from "./projectKnowledgeService.ts";
+} from "./projectKnowledgeService";
 
 dotenv.config();
 
