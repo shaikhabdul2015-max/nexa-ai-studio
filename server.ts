4fdc889f57f1999b1bdf2b983e570bfe3f6077ab
shaikhabdul2015-max <shaikhabdul2015@gmail.com>
2026-09-19 12:04:21 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -16,7 +16,7 @@ import {
   toggleProjectFileEnabled,
   deleteProjectFile,
   buildProjectPromptContext,
-} from "./src/services/ProjectKnowledgeService";
+} from"./src/services/ProjectKnowledgeService";
 dotenv.config();
 
 // Multer in-memory upload handler for audio payloads
