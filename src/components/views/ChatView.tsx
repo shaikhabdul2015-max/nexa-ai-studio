fb7c035bd33b0c541b44750493b9c4257d678723
shaikhabdul2015-max <shaikhabdul2015@gmail.com>
2026-09-23 09:51:41 +0530

    Update ChatView.tsx

--- src/components/views/ChatView.tsx
+++ src/components/views/ChatView.tsx
@@ -25,7 +25,7 @@ import {
   Terminal,
 } from "lucide-react";
 import { ChatAttachment, ChatMessage, AIModel } from "../../types";
-import{sendReasoningRequest,}from "../../services/api";
+import { sendReasoningRequest , }from "../../services/api";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
